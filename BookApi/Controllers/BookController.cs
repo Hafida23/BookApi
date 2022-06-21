6f8439f23a0c7af05f6ba0b51881816aaf463093
Hafida GAOUGAOU <PSR10568@students.ephec.be>
2022-06-21 10:52:34 +0200

    Add project files.

---
+++ BookApi/Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using BookApi.Data;
+using BookApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookServices _bookservices;
+        public BookController(BookServices bookservices)
+        {
+            _bookservices = bookservices;
+        }
+
+        [HttpGet("get-all-books")]  
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _bookservices.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVM book)
+        {
+            _bookservices.AddBook(book);
+            return Ok(book);
+        }
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+           var book= _bookservices.GetBookById(id);  
+           
+            return Ok(book);
+        }
+        [HttpDelete("deleteBook/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+             _bookservices.DeleteBookById(id);
+
+            return Ok();
+        }
+
+    }
+}
---
+++ BookApi/Services/BookServices.cs
@@ -0,0 +1,55 @@
+﻿using BookApi.Data;
+using BookApi.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookApi.Services
+{
+    public class BookServices
+    {
+        private readonly ApplicationDbContext _context;
+        public BookServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.DateRead,
+                Rate = book.Rate,
+                Genre = book.Genre,
+                Author = book.Author,
+                DateAdded = book.DateAdded
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+        public Book GetBookById(int id)
+        {
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
+            return book;
+        }
+        public void DeleteBookById(int id)
+        {
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges(true);
+            }
+           
+            
+        }
+
+    }
+}
